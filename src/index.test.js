a86245bcac74c250eee71d3367155cf9692c375c
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2024-08-24 02:49:55 +0300

    Create test for isSunk method

--- src/index.test.js
+++ src/index.test.js
@@ -1,16 +1,20 @@
 import { Ship } from './ship';
 
-const isSunkMock = jest.fn().mockReturnValue(false);
-
 test('create a ship object with the correct input', () => {
   const ship = new Ship(5);
   expect(ship.length).toBe(5);
   expect(ship.health).toBe(5);
-  expect(ship.isSunk).toBe(isSunkMock());
+  expect(ship.status).toBe('floating');
 });
 
 test('damage the the ship by 1', () => {
   const ship = new Ship(5);
   ship.hit();
   expect(ship.health).toBe(4);
+});
+
+test('Check if ship sunk or not', () => {
+  const ship = new Ship(1);
+  ship.hit();
+  expect(ship.status).toBe('sunk');
 });
