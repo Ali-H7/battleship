e67be91951a40a868cda47356fa1485175a96ed8
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2024-08-24 02:28:52 +0300

    Create test for hit method

--- src/index.test.js
+++ src/index.test.js
@@ -8,3 +8,9 @@ test('create a ship object with the correct input', () => {
   expect(ship.health).toBe(5);
   expect(ship.isSunk).toBe(isSunkMock());
 });
+
+test('damage the the ship by 1', () => {
+  const ship = new Ship(5);
+  ship.hit();
+  expect(ship.health).toBe(4);
+});
