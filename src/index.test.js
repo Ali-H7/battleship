f196e788f6d1ba7bb1b26104af663821f3bd5f81
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2024-08-26 01:24:33 +0300

    Create test to check if the gameboard create the ships

--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,21 @@
 import { Ship } from './ship';
+import { Gameboard } from './gameboard';
 
 test('create a ship object with the correct input', () => {
-  const ship = new Ship(5);
+  const ship = new Ship(0, 5);
   expect(ship.length).toBe(5);
   expect(ship.health).toBe(5);
   expect(ship.status).toBe('floating');
 });
 
 test('damage the the ship by 1', () => {
-  const ship = new Ship(5);
+  const ship = new Ship(0, 5);
   ship.hit();
   expect(ship.health).toBe(4);
 });
 
 test('check if ship sunk or not', () => {
-  const ship = new Ship(1);
+  const ship = new Ship(0, 1);
   ship.hit();
   expect(ship.status).toBe('sunk');
 });
@@ -25,4 +26,9 @@ test('create a gameboard object that contains a 10x10 board', () => {
   gameBoard.board.forEach((row) => {
     expect(row).toHaveLength(10);
   });
+});
+
+test('the gameboard object should create 10 ships', () => {
+  const gameBoard = new Gameboard();
+  expect(gameBoard.ships).toHaveLength(10);
 });
