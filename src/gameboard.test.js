11442c9883cc5f6bd1ea300807ec5bc0dcc6534e
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-10-07 09:34:33 +0300

    Add test for creating gameboard

---
+++ src/gameboard.test.js
@@ -0,0 +1,11 @@
+import Gameboard from "./gameboard";
+
+const gameboard = new Gameboard
+
+test('create an object 10x10 board', () => {
+    expect(gameboard.board).toHaveLength(10);
+    for (i = 0; i < 10; i++) {
+        expect(gameboard.board[i]).toHaveLength(10);
+    }
+});
+
